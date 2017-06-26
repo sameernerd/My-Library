5acd760bd727e80f6baa021a70a4766e24e97656
sameernerd <sameernerd@gmail.com>
2017-06-26 16:08:47 +0530

    Update CeiIndex.java

--- Searching Algorithms/CeiIndex.java
+++ Searching Algorithms/CeiIndex.java
@@ -1,16 +1,12 @@
-static int CeilIndex(int ar[],int l,int r,int x){
-		int m,index;
-		while(l<=r){
-			m=l+(r-l)/2;
-			if(x>ar[r])return -1;
-			if(x==ar[m])return m;
-			else if(x<=ar[m]){
-				r=m;
-				return r;
-			}
-			else{
-				l=m;
-			}
+static int ceilIndex(int ar[],int l,int r,int x,int o[]){
+	int m;
+	while(r-l>1){
+		m=(l+r)/2;
+		if(x==o[ar[m]])return m;
+		else if(x<=o[ar[m]]){
+			r=m;
 		}
-		return -1;
+		else l=m;
 	}
+	return r;
+}
