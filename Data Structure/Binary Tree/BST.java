68ed0353a0d6d47074c293d6552e219bb2bf3302
Sameer Raj <sameernerd@gmail.com>
2017-07-23 22:37:53 +0530

    Update BST.java

--- Data Structure/Binary Tree/BST.java
+++ Data Structure/Binary Tree/BST.java
@@ -29,29 +29,6 @@ public class BinaryTree {
 		return root;
 	}
 	
-	//Visit->Left->Right
-	public void Preorder(Node root){
-		if(root==null)return;
-		else System.out.println(root.data);
-		Preorder(root.left);
-		Preorder(root.right);
-	}
-	
-	//Left->Visit->Right
-	public void Inorder(Node root){
-		if(root==null)return;
-		Inorder(root.left);
-		System.out.println(root.data);
-		Inorder(root.right);
-	}
-	
-    //Left->Right->Visit
-	public void Postorder(Node root){
-		if(root==null)return;
-		Postorder(root.left);
-		Postorder(root.right);
-		System.out.println(root.data);
-	}
 	
 	//Worst case for searching is O(n) in regular BST and O(logn) in Balanced BST
 	public Node search(int data,Node root){
