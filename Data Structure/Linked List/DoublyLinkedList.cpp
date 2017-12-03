0add4926f3f9c7871b0cd25444c5042ab3dcf897
Sameer Raj <sameernerd@gmail.com>
2017-12-04 00:12:14 +0530

    Update DoublyLinkedList.cpp

--- Data Structure/Linked List/DoublyLinkedList.cpp
+++ Data Structure/Linked List/DoublyLinkedList.cpp
@@ -9,8 +9,12 @@ class LinkedList{
     	Node *next;
     	Node *prev;
 	};
+	Node *root;
+	LinkedList(){
+		root=NULL;
+	}
 	//Inserting at the last of Linked List
-	  Node* insertLast(Node *root,int data){
+	  void insertLast(int data){
 	  	Node *temp;
 		temp=root;
 		if(root==NULL){
@@ -18,104 +22,103 @@ class LinkedList{
 			root->data=data;
 			root->next=NULL;
 			root->prev=NULL;
-			return root;
 		}
-		while(temp->next!=NULL){
+		else{
+			while(temp->next!=NULL){
 			temp=temp->next;
+			}
+			Node *newNode=new Node();
+			newNode->data=data;
+			newNode->next=NULL;
+			newNode->prev=temp;
+			temp->next=newNode;	
 		}
-		Node *newNode=new Node();
-		newNode->data=data;
-		newNode->next=NULL;
-		newNode->prev=temp;
-		temp->next=newNode;
-		return root;
 	}
 	//Traversing the linked list
-	void traverse(Node *root){
-		while(root!=NULL){
-			cout<<root->data<<" ";
-			root=root->next;
+	void traverse(){
+		Node *temp=root;
+		while(temp!=NULL){
+			cout<<temp->data<<" ";
+			temp=temp->next;
 		}
 		cout<<endl;
 	}
 	//Inserting at the beginning of linked list
-	Node* insertFirst(Node *root,int data){
+	void insertFirst(int data){
 		if(root==NULL){
 			root=new Node();
 			root->data=data;
 			root->next=NULL;
 			root->prev=NULL;
-			return root;
 		}
-		Node *newNode=new Node();
-		newNode->data=data;
-		newNode->next=root;
-		newNode->prev=NULL;
-		root=newNode;
-		return root;
+		else{
+			Node *newNode=new Node();
+			newNode->data=data;
+			newNode->next=root;
+			newNode->prev=NULL;
+			root=newNode;
+		}
 	}
 	//Deleting first node
-	Node* deleteFirst(Node *root){
-		Node *newRoot;
-		newRoot=root->next;
-		root=NULL;
-		return newRoot;
+	void deleteFirst(){
+		if(root==NULL){
+			cout<<"Linked List is empty";
+		}
+		else root=root->next;
 	}
 	//Deleting the last node
-	Node* deleteLast(Node *root){
-		if(root==NULL)return NULL;
+	Node* deleteLast(){
+		Node *temp=root;
+		if(temp==NULL)return NULL;
 		Node *save=NULL;
-		while(root->next!=NULL){
-			save=root;
-			root=root->next;
+		while(temp->next!=NULL){
+			save=temp;
+			temp=temp->next;
 		}
 		if(save==NULL){
 			cout<<"Linked List Empty Now"<<endl;
 			root=NULL;
-			return root;
 		}
-		save->next=NULL;
+		else save->next=NULL;
 	}
 	//Deleting the node with item
-	Node* deleteItem(Node *root,int item){
-		if(root->data==item){
-			deleteFirst(root);
+	Node* deleteItem(int item){
+		Node *temp=root;
+		if(temp->data==item){
+			deleteFirst();
 		}
 		else{
-			Node *save,*temp=root;
+			Node *save;
 			while(temp!=NULL&&temp->data!=item){
 				save=temp;
 				temp=temp->next;
 			}
 			if(temp==NULL){
 				cout<<"Item Not Found";
-				return root;
 			}
-			temp->prev->next=temp->next;
+			else temp->prev->next=temp->next;
 		}
 	}
 };
 
 int main(){
 	LinkedList ll;
-	LinkedList::Node *root;
-	root=NULL;
-	root=ll.insertLast(root,14);
-	ll.insertLast(root,12);
-	ll.insertLast(root,44);
-	ll.traverse(root);
-	root=ll.insertFirst(root,16);
-	ll.traverse(root);
-	root=ll.deleteFirst(root);
-	root=ll.deleteFirst(root);
-	root=ll.deleteLast(root);
-	root=ll.deleteLast(root);
-	root=ll.insertFirst(root,67);
-	ll.insertLast(root,69);
-	ll.insertLast(root,38);
-	ll.insertLast(root,36);
-	ll.insertLast(root,68);
-	ll.traverse(root);
-	ll.deleteItem(root,68);
-	ll.traverse(root);
+	ll.insertLast(14);
+	ll.insertLast(12);
+	ll.insertLast(44);
+	ll.traverse();
+	ll.insertFirst(16);
+	ll.traverse();
+	ll.deleteFirst();
+	ll.deleteFirst();
+	ll.deleteLast();
+	ll.deleteLast();
+	ll.insertFirst(67);
+	ll.insertLast(69);
+	ll.insertLast(38);
+	ll.insertLast(36);
+	ll.insertLast(68);
+	ll.traverse();
+	ll.deleteItem(68);
+	ll.traverse();
 }
